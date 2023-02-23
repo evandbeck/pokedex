56b703b557c8718de76353da204df2b3352093dc
Evan Beck <evandrydenbeck@gmail.com>
2023-02-23 09:46:18 -0500

    updated images

--- src/PokeCard.js
+++ src/PokeCard.js
@@ -3,11 +3,15 @@ import PokeCardCSS from './PokeCard.css'
 
 function PokeCard({pokeCard}) {
     const { id, name, type, base_experience } = pokeCard;
+
+    const pokeAPI = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/"
+    const pokeID = String(id).padStart(3, '0')
+    let imgSrc = `${pokeAPI}${pokeID}.png`
     
   return (
     <div className="PokeCard">
         <h3>{name}</h3>
-        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} alt={name}/>
+        <img src={imgSrc} alt={name}/>
         <p>Type: {type}</p>
         <p>EXP: {base_experience}</p>
     </div>
