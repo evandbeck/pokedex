22eb382617836d01fbd142e763734a2101a7b9a6
Evan Beck <evandrydenbeck@gmail.com>
2023-02-24 11:26:17 -0500

    exp logic

--- src/PokeGame.js
+++ src/PokeGame.js
@@ -22,11 +22,14 @@ function PokeGame() {
         hand1.push(randPokemon);
     }
 
+    let exp1 = hand1.reduce((exp, pokemon) => exp + pokemon.base_experience, 0);
+    let exp2 = hand2.reduce((exp, pokemon) => exp + pokemon.base_experience, 0);
+
   return (
     <div>
         <div>PokeGame</div>
-        <Pokedex pokemon={hand1} />
-        <Pokedex pokemon={hand2} />
+        <Pokedex pokemon={hand1} exp={exp1} />
+        <Pokedex pokemon={hand2} exp={exp2} />
     </div>
   )
 }
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,13 +2,13 @@ import React from 'react';
 import PokeCard from './PokeCard';
 import PokedexCSS from './Pokedex.css';
 
-function Pokedex(pokemon) {
-  let pokeHand = pokemon.pokemon;
-  const pokeHands = pokeHand.map(pokeCard => <PokeCard key={pokeCard.id} pokeCard={pokeCard}/>)
+function Pokedex({pokemon, exp}) {
+  const pokeHands = pokemon.map(pokeCard => <PokeCard key={pokeCard.id} pokeCard={pokeCard}/>)
 
   return (
     <div>
         <h1>Pokedex</h1>
+        <p>Total Experience: {exp}</p>
         <div className="Pokedex-cards">
             {pokeHands}
         </div>
