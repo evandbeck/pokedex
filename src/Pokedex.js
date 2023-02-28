83a08def30dc9e9994fa3c8820ad64c7cab7ed67
Evan Beck <evandrydenbeck@gmail.com>
2023-02-27 19:07:44 -0500

    winner logic

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,13 +2,14 @@ import React from 'react';
 import PokeCard from './PokeCard';
 import PokedexCSS from './Pokedex.css';
 
-function Pokedex({pokemon, exp}) {
+function Pokedex({pokemon, exp, isWinner}) {
   const pokeHands = pokemon.map(pokeCard => <PokeCard key={pokeCard.id} pokeCard={pokeCard}/>)
 
   return (
     <div>
         <h1>Pokedex</h1>
         <p>Total Experience: {exp}</p>
+        <p>{isWinner ? "WINNER!" : "LOSER!"}</p>
         <div className="Pokedex-cards">
             {pokeHands}
         </div>
